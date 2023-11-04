0521fd8ca82ddcf06b61aefe6d143620b280b354
k15r <git@riddance.k15r.xyz>
2023-11-04 17:24:08 +0100

    Clean up.

--- bin/riddance_env.dart
+++
@@ -1,5 +0,0 @@
-import 'package:riddance_env/riddance_env.dart' as riddance_env;
-
-void main(List<String> arguments) {
-  print('Hello world: ${riddance_env.calculate()}!');
-}
--- test/riddance_env_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:riddance_env/riddance_env.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
