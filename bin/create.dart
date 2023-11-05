9112b66232d9baedee75efe6722f7ca2574001ce
k15r <git@riddance.k15r.xyz>
2023-11-05 22:12:35 +0100

    Initialize git.

--- bin/create.dart
+++ bin/create.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:riddance_env/git.dart';
 import 'package:riddance_env/pub.dart';
 
 void main(List<String> arguments) async {
   final path = Directory.current;
   await makePubspecYaml(path, arguments);
+  init(path);
 }
---
+++ lib/git.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:riddance_env/siblings.dart';
+
+void init(Directory path) {
+  final gitPath =
+      '${path.path}${Platform.pathSeparator}.git${Platform.pathSeparator}';
+  Directory('${gitPath}objects').createSync(recursive: true);
+  Directory('${gitPath}refs').createSync(recursive: true);
+  File('${gitPath}HEAD').writeAsStringSync('ref: refs/heads/main\n');
+  final config = vote(
+    path,
+    '.git${Platform.pathSeparator}config',
+    (content) =>
+        // ignore: prefer_interpolation_to_compose_strings
+        '[' +
+        content
+            .split('[')
+            .where((element) => element.startsWith('remote "origin"]'))
+            .join('[')
+            .replaceAllMapped(
+              _urlRegExp,
+              (match) => '${match.group(1)}%${match.group(3)}',
+            ),
+  );
+  // spell-checker: ignore repositoryformatversion filemode logallrefupdates
+  File('${gitPath}config').writeAsStringSync('''
+[core]
+	repositoryformatversion = 0
+	filemode = false
+	bare = false
+	logallrefupdates = true
+${config?.replaceAll('/%.git', '/${basename(path.path)}.git') ?? ''}[branch "main"]
+	remote = origin
+	merge = refs/heads/main''');
+}
+
+final _urlRegExp = RegExp(r'(url\s*=.*/)([^/]+)(\.git\n)');
